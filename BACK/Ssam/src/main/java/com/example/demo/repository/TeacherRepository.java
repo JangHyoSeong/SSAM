077ff31d4c5cda33041352cc5400515e59fc4054
JHS <wkdgytmd200@naver.com>
2024-07-19 17:23:19 +0900

    FIX : Fix Entity Bug

--- BACK/Ssam/src/main/java/com/example/demo/repository/TeacherRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.domain.user.teacher.entity.Teacher;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TeacherRepository extends JpaRepository<Teacher, Long> {
-    Boolean existsByUsername(String username);
-}

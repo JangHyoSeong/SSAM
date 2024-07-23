a3988a50e35024fdcb3cb125086d3cbf812aed68
JJH <asz232@naver.com>
2024-07-23 17:38:30 +0900

    fix: fix front-end project for test

--- FE/Frontend/src/mainPage/Explan.jsx
+++ FE/Frontend/src/mainPage/Explan.jsx
@@ -5,7 +5,7 @@ import explan from "./Explan.module.scss";
 const Explan = () => {
   return (
     <div className={explan.explanArray}>
-      <h1>새로 만나는 화상상담</h1>
+      <h1>새로 만나는 화상상담222222</h1>
       <h2>환영합니다 SSAM입니다</h2>
     </div>
   );

7c44fec82074cdb218c59b1a0cbe4f1eed32dde7
JJH <asz232@naver.com>
2024-07-29 10:22:55 +0900

    chore: remove project working directory

--- BACK/Ssam/README.md
+++
@@ -1,6 +0,0 @@
-# Ssam Spring
-
-## 개발 환경
-- JDK 17
-- Spring boot 3.3.1
-- SpringSecurity Security 6.2.1
--- BACK/Ssam/src/main/java/com/ssafy/ssam/domain/classroom/repository/UserBoardRelationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.ssam.domain.classroom.repository;
-
-import com.ssafy.ssam.domain.classroom.entity.UserBoardRelation;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserBoardRelationRepository extends JpaRepository<UserBoardRelation, Integer> {
-}

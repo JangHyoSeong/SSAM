14979abcfe83ede6331ed9a024fbc61e43487d75
정유진 <renascor96@naver.com>
2024-07-29 09:45:40 +0900

    style:승인,거절 modal 변경

--- FE/Frontend/src/teacher/classroom/TeacherApproveModal.jsx
+++ FE/Frontend/src/teacher/classroom/TeacherApproveModal.jsx
@@ -1,18 +1,18 @@
-import { useState } from 'react';
+import React from 'react';
 import styles from './TeacherApproveModal.module.scss';
 
-const TeacherApproveModal = ({ onClose, onApprove }) => {
+const TeacherApprovalModal = ({ onClose, onApprove }) => {
   return (
     <div className={styles.modalBackdrop}>
       <div className={styles.modalContent}>
         <h3>승인하시겠습니까?</h3>
         <div className={styles.buttonContainer}>
-          <button className={styles.approveButton} onClick={onApprove}>승인</button>
-          <button className={styles.cancelButton} onClick={onClose}>취소</button>
+          <button className={`${styles.button} ${styles.approveButton}`} onClick={onApprove}>승인</button>
+          <button className={`${styles.button} ${styles.cancelButton}`} onClick={onClose}>취소</button>
         </div>
       </div>
     </div>
   );
 };
 
-export default TeacherApproveModal;
+export default TeacherApprovalModal;

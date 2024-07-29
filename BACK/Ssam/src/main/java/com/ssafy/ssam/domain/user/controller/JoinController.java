7a1b9e50d02c6ec37bf06dfd9e3738cf737c7a52
JHS <wkdgytmd200@naver.com>
2024-07-29 11:35:08 +0900

    * TEST : Add Test code
    * test : 통합 테스트를 위한 코드 추가

--- BACK/Ssam/src/main/java/com/ssafy/ssam/domain/user/controller/JoinController.java
+++ BACK/Ssam/src/main/java/com/ssafy/ssam/domain/user/controller/JoinController.java
@@ -1,6 +1,8 @@
 package com.ssafy.ssam.domain.user.controller;
 
 import com.ssafy.ssam.domain.user.dto.UserDto;
+import com.ssafy.ssam.domain.user.entity.User;
+import com.ssafy.ssam.domain.user.repository.UserRepository;
 import com.ssafy.ssam.domain.user.service.JoinService;
 import com.ssafy.ssam.global.dto.CommonResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -11,12 +13,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Optional;
+
 @RequiredArgsConstructor
 @Controller
 @ResponseBody
 @RequestMapping("/v1/auth")
 public class JoinController{
     private final JoinService joinService;
+    private final UserRepository userRepository;
 
     @PostMapping("/students")
     public ResponseEntity<CommonResponseDto> studentJoinProcess(UserDto userDto){
@@ -29,9 +34,10 @@ public class JoinController{
     }
 
     @GetMapping("/test")
-    public ResponseEntity<CommonResponseDto> testGet() {
-        CommonResponseDto dto = new CommonResponseDto();
-        dto.setMessage("testOk");
-        return ResponseEntity.ok(dto);
+    public ResponseEntity<Optional<User>> testGet() {
+
+        Optional<User> userDto = userRepository.findById(1);
+
+        return ResponseEntity.ok(userDto);
     }
 }

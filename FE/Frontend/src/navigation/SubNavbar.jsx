5836bbddf6806dcb3afbbd301eaab04104ca2f74
kdhn97 <ehdgus3726@naver.com>
2024-07-31 11:26:57 +0900

    MODIFY: arlam

--- FE/Frontend/src/navigation/SubNavbar.jsx
+++ FE/Frontend/src/navigation/SubNavbar.jsx
@@ -1,12 +1,15 @@
 import { NavLink } from "react-router-dom";
 import styles from "./SubNavbar.module.scss";
+import alram from "../assets/alram.png";
 
 const SubNavbar = () => {
   return (
     <div className={styles.navArray}>
-      <NavLink to='/teacherupdate' className={styles.nav}>회원정보</NavLink>
-      |
-      <p>로그아웃</p>
+      <img src={alram} className={styles.alram}/>
+      <NavLink to="/teacherupdate" className={styles.nav}>
+        회원정보
+      </NavLink>
+      |<p>로그아웃</p>
     </div>
   );
 };

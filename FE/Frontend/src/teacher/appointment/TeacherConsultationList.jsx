6da1fcbe4b6a24f5f94fd27633884139a493ffd4
박범준 <fizd7@naver.com>
2024-07-31 12:11:38 +0900

    feat: add TeacherConsultationList

--- FE/Frontend/src/teacher/appointment/TeacherConsultationList.jsx
+++ FE/Frontend/src/teacher/appointment/TeacherConsultationList.jsx
@@ -4,7 +4,7 @@ import styles from "./TeacherConsultationList.module.scss";
 const TeacherConsultationList = () => {
   return (
     <div className={styles.consultationlistContainer}>
-      <div className={styles.classNavbar}>
+      <nav className={styles.classNavbar}>
         <NavLink
           to="/teacherreservationmanagement"
           className={({ isActive }) =>
@@ -21,8 +21,25 @@ const TeacherConsultationList = () => {
         >
           상담 목록
         </NavLink>
-      </div>
-      hello
+      </nav>
+      <section className={styles.consultationSection}>
+        <header className={styles.headerRow}>
+          <h3>상담 날짜</h3>
+          <h3>상담 시간</h3>
+          <h3>학생 이름</h3>
+          <h3>주제</h3>
+          <h3>내용</h3>
+        </header>
+        <article className={styles.consultationRow}>
+          <div>2024-07-15</div>
+          <div>15:00 ~ 15:20</div>
+          <div>박범준</div>
+          <div>학교 생활</div>
+          <div>우리 아이가 잘 지내고 있는지 궁금해요</div>
+          <button className={styles.approveButton}>승인</button>
+          <button className={styles.editButton}>거절</button>
+        </article>
+      </section>
     </div>
   );
 };

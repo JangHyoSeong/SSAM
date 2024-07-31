1921475eff6d51f7dc9710486c7ab373d8ff9510
JHS <wkdgytmd200@naver.com>
2024-07-31 16:57:29 +0900

    FIX : fix merge conflict
    * fix : merge conflict 수정

--- BACK/Ssam/src/main/java/com/ssafy/ssam/domain/classroom/repository/UserBoardRelationRepository.java
+++ BACK/Ssam/src/main/java/com/ssafy/ssam/domain/classroom/repository/UserBoardRelationRepository.java
@@ -7,10 +7,12 @@ import com.ssafy.ssam.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserBoardRelationRepository extends JpaRepository<UserBoardRelation, Integer> {
     Optional<UserBoardRelation> findByUserAndBoard(User user, Board board);
     Optional<UserBoardRelation> findByBoardAndStatus(Board board, UserBoardRelationStatus status);
+    Optional<List<UserBoardRelation>> findUserBoardRelationsByUser(User user);
 }

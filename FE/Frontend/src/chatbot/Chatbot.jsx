0da3e9a6d1f781d942f7fde1e2da2a8eb321cddb
kdhn97 <ehdgus3726@naver.com>
2024-07-31 16:56:49 +0900

    FEAT: Chatbot

--- FE/Frontend/src/chatbot/Chatbot.jsx
+++ FE/Frontend/src/chatbot/Chatbot.jsx
@@ -2,14 +2,27 @@ import { useState } from "react";
 import styles from "./Chatbot.module.scss";
 import SSAM from "../assets/SSAM.png";
 import chatbotImg from "../assets/chatbot.png";
-import book from "../assets/book.png";
+import book from "../assets/bookblue.png";
 import chat from "../assets/chat.png";
+import ChatbotChatModal from "./ChatbotChatModal";
 
 const Chatbot = () => {
   const [isBoxVisible, setIsBoxVisible] = useState(false);
+  const [isModalVisible, setIsModalVisible] = useState(false);
 
   const toggleBoxVisibility = () => {
     setIsBoxVisible(!isBoxVisible);
+    if (isBoxVisible && isModalVisible) {
+      setIsModalVisible(false);
+    }
+  };
+
+  const openModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const closeModal = () => {
+    setIsModalVisible(false);
   };
 
   return (
@@ -23,18 +36,23 @@ const Chatbot = () => {
         <div className={styles.formBox}>
           <img src={SSAM} className={styles.logo} />
           <hr />
-          <div className={styles.chatContent}></div>
+          <div className={styles.chatContent}>
+            <p>이곳은 학습방입니다</p>
+          </div>
           <div className={styles.chatMenu}>
             <div className={styles.imgBox}>
               <img src={book} className={styles.img} />
-              <p>학습</p>
+              <p className={styles.studyTxt}>학습</p>
             </div>
-            <div className={styles.imgBox}>
+            <div className={styles.imgBox} onClick={openModal}>
               <img src={chat} className={styles.img} />
               <p>대화</p>
             </div>
           </div>
         </div>
+      )}
+      {isModalVisible && (
+        <ChatbotChatModal isVisible={isModalVisible} closeModal={closeModal} />
       )}
     </div>
   );

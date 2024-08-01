069aeff049850b0109613003080682631f975e80
박범준 <fizd7@naver.com>
2024-08-01 23:38:57 +0900

    refactor-loginAuth

--- FE/Frontend/src/apis/user.jsx
+++ FE/Frontend/src/apis/user.jsx
@@ -8,3 +8,16 @@
 //   console.log("getUser");
 //   axios.get(`/user/${nickname}`).then(success).catch(fail);
 // };
+
+// import axiosConfig from "../utils/axiosConfig";
+
+import axios from "axios";
+
+const loginUser = (username, password) => {
+  return axios.post("http://localhost:8081/v1/auth/login", {
+    username: username,
+    password: password,
+  });
+};
+
+export { loginUser };
--- FE/Frontend/src/utils/axiosConfig.jsx
+++ FE/Frontend/src/utils/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosConfig = axios.create({
   withCredentials: true,
-  baseURL: "http://localhost:8081/",
+  baseURL: "http://localhost:8081/api/v1",
 });
 
 export default axiosConfig;

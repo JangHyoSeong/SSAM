6637c6c1fb3926723da6a7e0a2006a171eddda87
JHS <wkdgytmd200@naver.com>
2024-08-02 00:58:41 +0900

    FEAT : Add user info modification feature
    * feat : 유저 정보 수정 기능 추가

--- BACK/Ssam/src/main/java/com/ssafy/ssam/domain/user/controller/UserInfoController.java
+++ BACK/Ssam/src/main/java/com/ssafy/ssam/domain/user/controller/UserInfoController.java
@@ -1,12 +1,13 @@
 package com.ssafy.ssam.domain.user.controller;
 
+import com.ssafy.ssam.domain.user.dto.request.UserInfoModificationRequestDTO;
 import com.ssafy.ssam.domain.user.dto.response.UserInfoResponseDTO;
 import com.ssafy.ssam.domain.user.service.UserInfoService;
+import com.ssafy.ssam.global.dto.CommonResponseDto;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RequestMapping("/v1/users")
@@ -19,6 +20,11 @@ public class UserInfoController {
     @GetMapping
     public ResponseEntity<UserInfoResponseDTO> getUserInfo () {
         return ResponseEntity.ok(userInfoService.getUserInfo());
+    }
+
+    @PutMapping
+    public ResponseEntity<CommonResponseDto> modificateUserInfo (@Valid UserInfoModificationRequestDTO requestDTO) {
+        return ResponseEntity.ok(userInfoService.modificateUserInfo(requestDTO));
     }
 
 }
--- BACK/Ssam/src/main/java/com/ssafy/ssam/domain/user/service/UserInfoService.java
+++ BACK/Ssam/src/main/java/com/ssafy/ssam/domain/user/service/UserInfoService.java
@@ -1,16 +1,21 @@
 package com.ssafy.ssam.domain.user.service;
 
+import com.ssafy.ssam.domain.classroom.repository.SchoolRepository;
+import com.ssafy.ssam.domain.user.dto.request.UserInfoModificationRequestDTO;
 import com.ssafy.ssam.domain.user.dto.response.UserInfoResponseDTO;
+import com.ssafy.ssam.global.amazonS3.service.S3ImageService;
 import com.ssafy.ssam.global.auth.dto.CustomUserDetails;
 import com.ssafy.ssam.global.auth.entity.User;
 import com.ssafy.ssam.domain.classroom.entity.School;
 import com.ssafy.ssam.global.auth.repository.UserRepository;
+import com.ssafy.ssam.global.dto.CommonResponseDto;
 import com.ssafy.ssam.global.error.CustomException;
 import com.ssafy.ssam.global.error.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Optional;
 
@@ -19,6 +24,8 @@ import java.util.Optional;
 public class UserInfoService {
 
     private final UserRepository userRepository;
+    private final SchoolRepository schoolRepository;
+    private final S3ImageService s3ImageService;
 
 
     // 사용자 상세 정보 제공 로직
@@ -39,6 +46,32 @@ public class UserInfoService {
                 .selfPhone(user.getPhone())
                 .otherPhone(user.getOtherPhone())
                 .build();
+    }
+
+    // 사용자 정보 수정 로직
+    public CommonResponseDto modificateUserInfo (UserInfoModificationRequestDTO requestDTO) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+
+        User user = userRepository.findByUserId(userDetails.getUserId())
+                .orElseThrow(() -> new CustomException(ErrorCode.UserNotFoundException));
+
+        String schoolName = requestDTO.getSchool();
+        String selfPhone = requestDTO.getSelfPhone();
+        String otherPhone = requestDTO.getOtherPhone();
+        MultipartFile profileImage = requestDTO.getProfileImage();
+
+        user.setSchool(schoolRepository.findSchoolByName(schoolName).orElse(null));
+        user.setPhone(selfPhone);
+        user.setOtherPhone(requestDTO.getOtherPhone());
+        if (profileImage != null && !profileImage.isEmpty()) {
+            String imagePath = s3ImageService.upload(profileImage);
+            user.setImgUrl(imagePath);
+        }
+
+        userRepository.save(user);
+
+        return new CommonResponseDto("Modificated");
     }
 
 }

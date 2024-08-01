803ba0a7b7afa45efa768342a1ba63079e7a4c78
kdhn97 <ehdgus3726@naver.com>
2024-08-01 11:12:09 +0900

    MERGE

--- FE/Frontend/src/teacher/myPage/TeacherUpdate.jsx
+++ FE/Frontend/src/teacher/myPage/TeacherUpdate.jsx
@@ -1,14 +1,12 @@
 // 선생님 정보 수정 페이지 컴포넌트
-import { NavLink } from 'react-router-dom';
-import styles from './TeacherUpdate.module.scss';
+import { NavLink } from "react-router-dom";
+import styles from "./TeacherUpdate.module.scss";
 
 const TeacherUpdate = () => {
   return (
     <div className={styles.Container}>
       <div className={styles.menuNavbar}>
-        <div className={styles.updateItem}>
-          회원정보 수정
-        </div>
+        <div className={styles.updateItem}>회원정보 수정</div>
         <NavLink to="/teacherpasswordchange" className={styles.changeItem}>
           비밀번호 변경
         </NavLink>
@@ -21,17 +19,35 @@ const TeacherUpdate = () => {
               <td className={styles.imgTd}>
                 <div className={styles.profileImg}></div>
                 <div className={styles.btn}>
-                  <button className={styles.imgBtn}>수정</button>
+                  <input type="file" className={styles.imgBtn} />
                   <button className={styles.imgBtn}>삭제</button>
                 </div>
               </td>
             </tr>
-            <tr><th>이름</th><td></td></tr>
-            <tr><th>생년월일</th><td></td></tr>
-            <tr><th>학교</th><td></td></tr>
-            <tr><th>아이디</th><td></td></tr>
-            <tr><th>이메일</th><td></td></tr>
-            <tr><th>휴대전화</th><td></td></tr>
+            <tr>
+              <th>이름</th>
+              <td></td>
+            </tr>
+            <tr>
+              <th>생년월일</th>
+              <td></td>
+            </tr>
+            <tr>
+              <th>학교</th>
+              <td></td>
+            </tr>
+            <tr>
+              <th>아이디</th>
+              <td></td>
+            </tr>
+            <tr>
+              <th>이메일</th>
+              <td></td>
+            </tr>
+            <tr>
+              <th>휴대전화</th>
+              <td></td>
+            </tr>
           </table>
         </div>
         <div className={styles.formBtnArray}>

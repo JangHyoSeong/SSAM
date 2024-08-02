648757d662efeb2a0e5f3ec10c85213997499d23
kdhn97 <ehdgus3726@naver.com>
2024-08-02 11:26:51 +0900

    MERGE

--- FE/Frontend/src/components/teacher/subPage/TeacherSubpage.jsx
+++ FE/Frontend/src/components/teacher/subPage/TeacherSubpage.jsx
@@ -1,15 +1,9 @@
 import TeacherSelect from "./TeacherSelect";
-import Explan from "../../mainPage/Explan";
-import Direct from "../../mainPage/Direct";
-import Footer from "../../mainPage/Footer";
 
 const TeacherSubpage = () => {
   return (
     <>
       <TeacherSelect />
-      <Direct />
-      <Explan />
-      <Footer />
     </>
   );
 };

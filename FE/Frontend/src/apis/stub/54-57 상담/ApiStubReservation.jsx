05fb555b9948688d5bfd34184ea83f12b9fa8510
박범준 <fizd7@naver.com>
2024-08-05 16:33:38 +0900

    api-reservation

--- FE/Frontend/src/apis/stub/54-57 상담/ApiStubReservation.jsx
+++ FE/Frontend/src/apis/stub/54-57 상담/ApiStubReservation.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-const ApiStubReservation = () => {
+export const useApiStubReservationInfo = () => {
   const [userData, setUserData] = useState(null);
 
   useEffect(() => {
@@ -15,38 +15,38 @@ const ApiStubReservation = () => {
       })
       .then((response) => {
         setUserData(response.data);
+        console.log("userData", userData);
       })
       .catch((error) => {
         console.error("API 요청 중 에러 발생:", error);
       });
   }, []);
 
-  return (
-    <div>
-      {userData && (
-        <div>
-          <p>
-            <strong>ID:</strong> {userData.userId}
-          </p>
-          <p>
-            <strong>Username:</strong> {userData.username}
-          </p>
-          <p>
-            <strong>Name:</strong> {userData.name}
-          </p>
-          <p>
-            <strong>School:</strong> {userData.school}
-          </p>
-          <p>
-            <strong>Board ID:</strong> {userData.boardId}
-          </p>
-          <p>
-            <strong>Role:</strong> {userData.role}
-          </p>
-        </div>
-      )}
-    </div>
-  );
+  return { userData };
 };
 
-export default ApiStubReservation;
+// export const ApiStubReservationList = () => {
+//   try {
+//     const [userData, setUserData] = useState(null);
+
+//     useEffect(() => {
+//       const token = localStorage.getItem("USER_TOKEN");
+//       axios
+//         .get(`http://localhost:8081/v1//consults/${userData.userId}`, {
+//           headers: {
+//             "Content-Type": "application/json",
+//             Authorization: `${token}`,
+//           },
+//         })
+//         .then((response) => {
+//           setUserData(response.data);
+//         })
+//         .catch((error) => {
+//           console.error("API 요청 중 에러 발생:", error);
+//         });
+//     }, []);
+//     return;
+//   } catch (error) {
+//     console.error("");
+//   }
+// };

72c0bbf8b4e0a88d862a52fa66c061c7ed65d121
박범준 <fizd7@naver.com>
2024-08-05 15:04:19 +0900

    feat: ApiStubReservation

--- FE/Frontend/src/components/parents/subPage/ParentsSelect.jsx
+++ FE/Frontend/src/components/parents/subPage/ParentsSelect.jsx
@@ -4,10 +4,10 @@ import select from "./ParentsSelect.module.scss";
 import classroom from "../../../assets/classroom.png";
 import question from "../../../assets/question.png";
 import appointment from "../../../assets/appointment.png";
-import pinStore from "../../../apis/stub/35-43 학급/apiStubPin";
+import PinStore from "../../../apis/stub/35-43 학급/apiStubPin";
 
 const Select = () => {
-  const { pin, fetchPinData } = pinStore();
+  const { pin, fetchPinData } = PinStore();
 
   const handleButtonClick = async () => {
     try {

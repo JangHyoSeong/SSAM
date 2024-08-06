bd9d6db2d482cf7beb62ece3b8a2aed9164837f5
kdhn97 <ehdgus3726@naver.com>
2024-08-06 17:56:23 +0900

    0806 SSAFY END

--- FE/Frontend/src/components/parents/subPage/ClassEnterModal.jsx
+++ FE/Frontend/src/components/parents/subPage/ClassEnterModal.jsx
@@ -3,39 +3,39 @@ import axios from "axios";
 import styles from "./ClassEnterModal.module.scss";
 
 const ClassEnterModal = () => {
+  // 상태 관리: PIN 번호를 입력받을 배열, 선택된 학급 정보, 모달의 표시 여부
   const [pins, setPins] = useState(Array(6).fill(""));
   const [classroom, setClassroom] = useState(null);
   const [isModalVisible, setIsModalVisible] = useState(true);
-  const inputRefs = useRef(new Array(6));
-  const pin = pins.join("");
+  const inputRefs = useRef(new Array(6)); // 입력창 참조 배열
+  const pin = pins.join(""); // PIN 번호를 하나의 문자열로 결합
 
+  // PIN 번호가 완성되면 학급 정보를 가져오는 함수
   useEffect(() => {
     const fetchClassroom = async () => {
       if (pin.length === 6) {
-        const token = localStorage.getItem("USER_TOKEN");
+        const token = localStorage.getItem("USER_TOKEN"); // 로컬 스토리지에서 토큰을 가져옴
         console.log("Using token: ", token);
-
         try {
           const response = await axios.get(
             "http://localhost:8081/v1/classrooms",
             {
-              pin: pin,
-            },
-            {
+              params: { pin: pin },
               headers: {
                 "Content-Type": "application/json",
-                Authorization: `${token}`,
+                Authorization: `Bearer ${token}`,
               },
             }
           );
           if (response.data && response.data.pin === pin) {
-            setClassroom(response.data);
+            setClassroom(response.data); // 학급 정보 설정
+            console.log(response.data);
           } else {
             console.error("No matching classroom found with the provided PIN");
             setClassroom(null);
           }
         } catch (error) {
-          console.error("Axios error: ", error.response || error); // Detailed error
+          console.error("Axios error: ", error.response || error);
           setClassroom(null);
         }
       }
@@ -43,15 +43,17 @@ const ClassEnterModal = () => {
     fetchClassroom();
   }, [pin]);
 
+  // PIN 입력 시 동작하는 함수
   const pinChange = (index) => (e) => {
     const newPins = [...pins];
-    newPins[index] = e.target.value.slice(0, 1);
+    newPins[index] = e.target.value.slice(0, 1); // 입력값을 1자리로 제한
     setPins(newPins);
     if (index < 5 && e.target.value) {
-      inputRefs.current[index + 1].focus();
+      inputRefs.current[index + 1].focus(); // 다음 입력창으로 포커스 이동
     }
   };
 
+  // 모달 닫기 함수
   const closeModal = () => {
     setIsModalVisible(false);
   };

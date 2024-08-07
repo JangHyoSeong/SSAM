b5ba7250fcdb0de88e9facc4ebeee3d1fbf86d97
정유진 <renascor96@naver.com>
2024-08-07 21:14:50 +0900

    refactor: TeacherClassroom 학생 불러오기

---
+++ FE/Frontend/src/apis/stub/35-43 학급/apiStubStudents.jsx
@@ -0,0 +1,19 @@
+// src/apis/stub/35-43 학급/apiStubStudents.jsx
+
+import axios from "axios";
+import { fetchApiUserInitial } from "../20-22 사용자정보/apiStubUserInitial";
+
+export const fetchUserData = async () => {
+  const token = localStorage.getItem("USER_TOKEN");
+  const { boardId } = await fetchApiUserInitial();
+  const response = await axios.get(
+    `http://localhost:8081/v1/classrooms/${boardId}`,
+    {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `${token}`,
+      },
+    }
+  );
+  return response.data;
+};

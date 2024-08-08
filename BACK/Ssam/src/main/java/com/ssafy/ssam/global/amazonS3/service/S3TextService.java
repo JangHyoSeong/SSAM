7d7ced0c7f18757ff5005d7606e039cdf074c956
KHG <skygprud0707@gmail.com>
2024-08-08 17:46:41 +0900

    FEAT : AWS Read File * 파일 읽어오기

--- BACK/Ssam/src/main/java/com/ssafy/ssam/global/amazonS3/service/S3TextService.java
+++ BACK/Ssam/src/main/java/com/ssafy/ssam/global/amazonS3/service/S3TextService.java
@@ -5,47 +5,39 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.GetObjectRequest;
 import com.amazonaws.services.s3.model.S3Object;
 import com.amazonaws.services.s3.model.S3ObjectInputStream;
+import com.ssafy.ssam.global.amazonS3.config.S3Config;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 @Slf4j
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class S3TextService {
-
     private final AmazonS3 amazonS3;
-
     @Value("${cloud.aws.s3.bucketName}")
     private String bucket;
 
     @Transactional
     public String readText(String sessionId){
+        S3Object o = amazonS3.getObject(bucket, sessionId+"/"+sessionId+".txt");
 
-        S3Object o = amazonS3.getObject(new GetObjectRequest(bucket, sessionId+"/"+sessionId));
-        StringBuilder sb=new StringBuilder();
-
-        try (S3ObjectInputStream ois = o.getObjectContent();
-            BufferedReader br = new BufferedReader (new InputStreamReader(ois, "UTF-8"))){
-            String line;
-            while ((line = br.readLine()) != null) {
-                sb.append(line);
-            }
-        } catch (UnsupportedEncodingException e) {
-            throw new RuntimeException(e);
+        StringBuilder sb = new StringBuilder();
+        String text;
+        try {
+            S3ObjectInputStream ois = o.getObjectContent();
+            text = new String(ois.readAllBytes(),StandardCharsets.UTF_8);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        System.out.println(sb.toString());
-        return sb.toString();
+        System.out.println(text);
+        return text;
     }
 
 

1c9f4018221ea9111b6f30f96adf38df3ff53322
KHG <skygprud0707@gmail.com>
2024-08-09 09:48:36 +0900

    MODIFY : Consult edit
    * consult findbyId -> findbyconsultId 로 수정

--- BACK/Ssam/src/main/java/com/ssafy/ssam/domain/consult/repository/ConsultRepository.java
+++ BACK/Ssam/src/main/java/com/ssafy/ssam/domain/consult/repository/ConsultRepository.java
@@ -11,7 +11,7 @@ import com.ssafy.ssam.domain.consult.entity.Consult;
 
 public interface ConsultRepository extends JpaRepository<Consult, Integer>{
     // Id 기반으로 존재하는지 여부 검증 jpa
-    Consult findById(int id);
+    Optional<Consult> findByConsultId(int id);
     Optional<Consult> findByAccessCode(String accessCode);
     // 약속을 컨설트로 변환하는 jpa
     List<Consult> findByAppointmentIn(List<Appointment> appointments);

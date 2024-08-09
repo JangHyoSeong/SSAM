68f32ee37b687e121af9f337fa7116cb7a7de234
kdhn97 <ehdgus3726@naver.com>
2024-08-09 23:38:21 +0900

    update

--- FE/Frontend/src/components/parents/join/ParentsJoin.jsx
+++ FE/Frontend/src/components/parents/join/ParentsJoin.jsx
@@ -8,6 +8,7 @@ import calendar from "../../../assets/calendar.png";
 import phone from "../../../assets/phone.png";
 import round1 from "../../../assets/round1.png";
 import round2 from "../../../assets/round2.png";
+import Swal from "sweetalert2";
 
 const apiUrl = import.meta.env.API_URL
 
@@ -30,14 +31,22 @@ const ParentsJoin = () => {
     });
   };
 
-  // 폼 제출 시 호출되는 함수
+  // 회원가입 POST
   const joinSubmit = (e) => {
     e.preventDefault();
     axios
       .post(`${apiUrl}/v1/auth/students`, formData)
       .then((response) => {
         console.log("axios 성공", response);
-        alert("성공");
+        Swal.fire({
+          title: "성공!",
+          text: "회원가입이 완료되었습니다",
+          icon: "success",
+        }).then(function (isConfirm) {
+          if (isConfirm) {
+            window.location.replace("./teacherlogin");
+          }
+        });
       })
       .catch((error) => {
         console.error("axios 실패", error);

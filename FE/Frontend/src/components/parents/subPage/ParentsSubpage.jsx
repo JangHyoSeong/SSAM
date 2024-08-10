326ccc413b7d5808ce04e62099be0cd52423e6a4
박범준 <fizd7@naver.com>
2024-08-10 17:41:44 +0900

    style: subPage-style

--- FE/Frontend/src/components/parents/subPage/ParentsSubpage.jsx
+++ FE/Frontend/src/components/parents/subPage/ParentsSubpage.jsx
@@ -1,11 +1,22 @@
 import EnterCode from "./EnterCode";
 import ParentsSelect from "./ParentsSelect";
+import SubpageImg1 from "../../../assets/subPage1.png";
+import styles from "./ParentsSubpage.module.scss";
 
 const ParentsSubpage = () => {
   return (
     <>
-      <hr />
-      <EnterCode />
+      <div className={styles.subPageContainer}>
+        <img
+          src={SubpageImg1}
+          className={styles.subpageImg}
+          alt="SubpageImg1"
+        />
+        <div className={styles.enterCodeContainer}>
+          <EnterCode />
+        </div>
+        <div className={styles.parentsSelectContainer}></div>
+      </div>
       <ParentsSelect />
     </>
   );

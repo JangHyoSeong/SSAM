1e2d187a208fe67f50ca78ac0614cf6e06625121
kdhn97 <ehdgus3726@naver.com>
2024-08-11 15:49:48 +0900

    MERGE

--- FE/Frontend/src/video/Video.jsx
+++ FE/Frontend/src/video/Video.jsx
@@ -15,55 +15,41 @@ import cameraOff from "../assets/cameraOff.png";
 const apiUrl = import.meta.env.API_URL;
 
 const VideoChatComponent = () => {
-  const { accessCode } = useParams(); // URL 파라미터에서 accessCode 가져오기
-  const [sessionId, setSessionId] = useState(null); // 세션 ID 상태 변수
-  const [session, setSession] = useState(null); // OpenVidu 세션 객체 상태 변수
-  const [mainStreamManager, setMainStreamManager] = useState(null); // 메인 스트림 관리자 상태 변수
-  const [publisher, setPublisher] = useState(null); // 퍼블리셔 상태 변수
-  const [subscribers, setSubscribers] = useState([]); // 구독자 리스트 상태 변수
-  const [currentVideoDevice, setCurrentVideoDevice] = useState(null); // 현재 비디오 장치 상태 변수
-  const [chatMessages, setChatMessages] = useState([]); // 채팅 메시지 상태 변수
-  const [chatInput, setChatInput] = useState(""); // 채팅 입력 상태 변수
-  const [isRecording, setIsRecording] = useState(false); // 녹화 여부 상태 변수
-  const [recordingId, setRecordingId] = useState(null); // 녹화 ID 상태 변수
-  const [isCameraOn, setIsCameraOn] = useState(true); // 카메라 상태 변수
-  const [isMicOn, setIsMicOn] = useState(true); // 마이크 상태 변수
-  const [time, setTime] = useState({ minutes: 0, seconds: 0 }); // 타이머 상태 변수
-  const OV = useRef(new OpenVidu()); // OpenVidu 객체 참조
-  const myUserName = useRef(`user_${Math.floor(Math.random() * 1000) + 1}`); // 임의의 사용자 이름 생성
+  const { accessCode } = useParams();
+  const [session, setSession] = useState(null);
+  const [mainStreamManager, setMainStreamManager] = useState(null);
+  const [publisher, setPublisher] = useState(null);
+  const [subscribers, setSubscribers] = useState([]);
+  const [currentVideoDevice, setCurrentVideoDevice] = useState(null);
+  const [chatMessages, setChatMessages] = useState([]);
+  const [chatInput, setChatInput] = useState("");
+  const [isRecording, setIsRecording] = useState(false);
+  const [isCameraOn, setIsCameraOn] = useState(true);
+  const [isMicOn, setIsMicOn] = useState(true);
+  const OV = useRef(new OpenVidu());
 
+  const myUserName = useRef(`user_${Math.floor(Math.random() * 1000) + 1}`);
+
+  // 컴포넌트가 마운트 될 때와 언마운트 될 때 이벤트 리스너 추가 및 제거
   useEffect(() => {
-    window.addEventListener("beforeunload", onBeforeUnload); // 창이 닫히기 전 이벤트 리스너 등록
-    joinSession(); // 세션 참여
-
-    // 타이머 설정
-    const timerInterval = setInterval(() => {
-      setTime((prevTime) => {
-        const newSeconds = prevTime.seconds + 1;
-        const newMinutes =
-          newSeconds >= 60 ? prevTime.minutes + 1 : prevTime.minutes;
-        return {
-          minutes: newMinutes,
-          seconds: newSeconds >= 60 ? 0 : newSeconds,
-        };
-      });
-    }, 1000);
-
+    window.addEventListener("beforeunload", onBeforeUnload);
+    joinSession();
     return () => {
-      window.removeEventListener("beforeunload", onBeforeUnload); // 이벤트 리스너 제거
-      leaveSession(); // 세션 종료
-      clearInterval(timerInterval); // 타이머 정리
+      window.removeEventListener("beforeunload", onBeforeUnload);
+      leaveSession();
     };
   }, []);
 
+  // 페이지를 떠나기 전 세션을 떠나는 함수
   const onBeforeUnload = () => {
-    leaveSession(); // 창이 닫히기 전 세션 종료
+    leaveSession();
   };
 
+  // 세션에 참가하는 함수
   const joinSession = async () => {
-    const mySession = OV.current.initSession(); // 세션 초기화
+    const mySession = OV.current.initSession();
 
-    // 새로운 스트림이 생성되었을 때 처리
+    // 새로운 스트림이 생성될 때 이벤트 리스너
     mySession.on("streamCreated", (event) => {
       if (
         event.stream.connection.connectionId !==
@@ -74,36 +60,35 @@ const VideoChatComponent = () => {
       }
     });
 
-    // 스트림이 파괴되었을 때 처리
+    // 스트림이 파괴될 때 이벤트 리스너
     mySession.on("streamDestroyed", (event) => {
       setSubscribers((subscribers) =>
         subscribers.filter((sub) => sub !== event.stream.streamManager)
       );
     });
 
-    // 예외 처리
+    // 예외가 발생할 때 이벤트 리스너
     mySession.on("exception", (exception) => {
       console.warn(exception);
     });
 
     try {
-      const token = await getToken(); // 토큰 가져오기
-      await mySession.connect(token, { clientData: myUserName.current }); // 세션에 연결
+      const token = await getToken();
+      await mySession.connect(token, { clientData: myUserName.current });
 
-      setSessionId(mySession.sessionId); // 세션 ID 설정
       let publisher = await OV.current.initPublisherAsync(undefined, {
         audioSource: undefined,
         videoSource: undefined,
         publishAudio: true,
         publishVideo: true,
-        resolution: "480x480",
+        resolution: "640x480",
         frameRate: 30,
         insertMode: "APPEND",
         mirror: false,
       });
-      mySession.publish(publisher); // 퍼블리셔 공개
+      mySession.publish(publisher);
 
-      const devices = await OV.current.getDevices(); // 장치 목록 가져오기
+      const devices = await OV.current.getDevices();
       const videoDevices = devices.filter(
         (device) => device.kind === "videoinput"
       );
@@ -114,10 +99,11 @@ const VideoChatComponent = () => {
       const currentVideoDevice = videoDevices.find(
         (device) => device.deviceId === currentVideoDeviceId
       );
-      setSession(mySession); // 세션 설정
-      setMainStreamManager(publisher); // 메인 스트림 관리자 설정
-      setPublisher(publisher); // 퍼블리셔 설정
-      setCurrentVideoDevice(currentVideoDevice); // 현재 비디오 장치 설정
+
+      setSession(mySession);
+      setMainStreamManager(publisher);
+      setPublisher(publisher);
+      setCurrentVideoDevice(currentVideoDevice);
     } catch (error) {
       console.log(
         "There was an error connecting to the session:",
@@ -127,6 +113,7 @@ const VideoChatComponent = () => {
     }
   };
 
+  // 세션을 떠나는 함수
   const leaveSession = async () => {
     if (session) {
       try {
@@ -139,17 +126,18 @@ const VideoChatComponent = () => {
       } catch (error) {
         console.error("Error deleting token:", error);
       }
-      session.disconnect(); // 세션 연결 종료
+      session.disconnect();
     }
-    setSession(null); // 세션 상태 초기화
-    setSubscribers([]); // 구독자 리스트 초기화
-    setMainStreamManager(null); // 메인 스트림 관리자 초기화
-    setPublisher(null); // 퍼블리셔 초기화
+    setSession(null);
+    setSubscribers([]);
+    setMainStreamManager(null);
+    setPublisher(null);
   };
 
+  // 카메라를 전환하는 함수
   const switchCamera = async () => {
     try {
-      const devices = await OV.current.getDevices(); // 장치 목록 가져오기
+      const devices = await OV.current.getDevices();
       const videoDevices = devices.filter(
         (device) => device.kind === "videoinput"
       );
@@ -167,11 +155,11 @@ const VideoChatComponent = () => {
             mirror: false,
           });
 
-          await session.unpublish(mainStreamManager); // 기존 스트림 발행 중지
-          await session.publish(newPublisher); // 새로운 스트림 발행
-          setCurrentVideoDevice(newVideoDevice[0]); // 새로운 비디오 장치 설정
-          setMainStreamManager(newPublisher); // 새로운 메인 스트림 관리자 설정
-          setPublisher(newPublisher); // 새로운 퍼블리셔 설정
+          await session.unpublish(mainStreamManager);
+          await session.publish(newPublisher);
+          setCurrentVideoDevice(newVideoDevice[0]);
+          setMainStreamManager(newPublisher);
+          setPublisher(newPublisher);
         }
       }
     } catch (e) {
@@ -179,60 +167,52 @@ const VideoChatComponent = () => {
     }
   };
 
+  // 녹화를 시작/중지하는 함수
   const toggleRecording = async () => {
     if (!isRecording) {
       try {
         const response = await axios.post(
           `${apiUrl}/v1/video/recording/start`,
           {
-            accessCode: accessCode,
+            sessionId: session.sessionId,
             outputMode: "COMPOSED",
             hasAudio: true,
             hasVideo: true,
           }
         );
-        setRecordingId(response.data.id); // 녹화 ID 설정
-        setIsRecording(true); // 녹화 상태 설정
+        setIsRecording(true);
       } catch (error) {
         console.error("Error starting recording:", error);
       }
     } else {
       try {
         await axios.post(`${apiUrl}/v1/video/recording/stop`, {
-          recordingId: recordingId,
+          sessionId: session.sessionId,
         });
-        setIsRecording(false); // 녹화 상태 해제
-        setRecordingId(null); // 녹화 ID 초기화
+        setIsRecording(false);
       } catch (error) {
         console.error("Error stopping recording:", error);
       }
     }
   };
 
-  // const toggleCamera = () => {
-  //   if (publisher) {
-  //     publisher.publishVideo(!isCameraOn); // 카메라 상태 전환
-  //     setIsCameraOn(!isCameraOn); // 카메라 상태 업데이트
-  //   }
-  // };
-
+  // 카메라를 켜고 끄는 함수
   const toggleCamera = () => {
-    setIsCameraOn((prevIsCameraOn) => {
-      const newIsCameraOn = !prevIsCameraOn;
-      if (publisher) {
-        publisher.publishVideo(newIsCameraOn);
-      }
-      return newIsCameraOn;
-    });
-  };
-
-  const toggleMic = () => {
     if (publisher) {
-      publisher.publishAudio(!isMicOn); // 마이크 상태 전환
-      setIsMicOn(!isMicOn); // 마이크 상태 업데이트
+      publisher.publishVideo(!isCameraOn);
+      setIsCameraOn(!isCameraOn);
     }
   };
 
+  // 마이크를 켜고 끄는 함수
+  const toggleMic = () => {
+    if (publisher) {
+      publisher.publishAudio(!isMicOn);
+      setIsMicOn(!isMicOn);
+    }
+  };
+
+  // 채팅 메시지를 보내는 함수
   const sendChatMessage = () => {
     if (chatInput.trim() !== "" && session) {
       const messageData = {
@@ -244,40 +224,37 @@ const VideoChatComponent = () => {
         data: JSON.stringify(messageData),
         type: "chat",
       });
-      setChatMessages((prevMessages) => [...prevMessages, messageData]); // 채팅 메시지 리스트 업데이트
-      setChatInput(""); // 채팅 입력 초기화
+      setChatMessages((prevMessages) => [...prevMessages, messageData]);
+      setChatInput("");
     }
   };
 
+  // 세션이 변경될 때 채팅 메시지를 수신하는 이벤트 리스너 추가
   useEffect(() => {
     if (session) {
       session.on("signal:chat", (event) => {
         const data = JSON.parse(event.data);
         if (data.connectionId !== session.connection.connectionId) {
-          setChatMessages((prevMessages) => [...prevMessages, data]); // 다른 사용자의 채팅 메시지 수신
+          setChatMessages((prevMessages) => [...prevMessages, data]);
         }
       });
     }
   }, [session]);
 
+  // 토큰을 가져오는 함수
   const getToken = async () => {
     try {
       const response = await axios.post(`${apiUrl}/v1/video/token`, {
         accessCode: accessCode,
         userId: myUserName.current,
       });
-      return response.data.token; // 토큰 반환
+      return response.data.token;
     } catch (error) {
       console.error("Error getting token:", error);
       throw error;
     }
   };
 
-  // 오늘 날짜 불러오기
-  const today = new Date();
-  const formattedDate = `${today.getFullYear()}년 ${String(
-    today.getMonth() + 1
-  ).padStart(2, "0")}월 ${String(today.getDate()).padStart(2, "0")}일`;
 
   return (
     <div className={styles.videoArray}>
@@ -292,7 +269,7 @@ const VideoChatComponent = () => {
               </div>
               {/* <h3>Session: {sessionId}</h3> */}
               <div className={styles.dayArray}>
-                <p>{formattedDate}</p>
+                {/* <p>{formattedDate}</p> */}
               </div>
               <div className={styles.iconArray}>
                 {/* 녹화 버튼 */}
@@ -341,11 +318,11 @@ const VideoChatComponent = () => {
 
           {/* 시간 */}
           <div className={styles.timeArray}>
-            <div className={styles.time}>
+            {/* <div className={styles.time}>
               <h1>{`${String(time.minutes).padStart(2, "0")}:${String(
                 time.seconds
               ).padStart(2, "0")}`}</h1>
-            </div>
+            </div> */}
           </div>
 
           {/* 화면 */}

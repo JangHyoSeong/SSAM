24fb7c8dd08306d6fca6a2375c3fe7bf67514b2e
JJH <asz232@naver.com>
2024-08-12 10:48:45 +0900

    feat: python 컨테이너 연결 Controller 추가

--- BACK/Ssam/src/main/java/com/ssafy/ssam/domain/profanity/service/ProfanityService.java
+++ BACK/Ssam/src/main/java/com/ssafy/ssam/domain/profanity/service/ProfanityService.java
@@ -1,31 +1,37 @@
-//package com.ssafy.ssam.domain.profanity.service;
-//
-//import org.springframework.http.HttpEntity;
-//import org.springframework.http.HttpHeaders;
-//import org.springframework.http.MediaType;
-//import org.springframework.stereotype.Service;
-//import org.springframework.web.client.RestTemplate;
-//
-//@Service
-//public class ProfanityService {
-//    private final RestTemplate restTemplate;
-//    private final String pythonServiceUrl = "http://localhost:8082/predict";
-//
-//    public PredictionResult getPrediction(String sentence) {
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.setContentType(MediaType.APPLICATION_JSON);
-//
-//        HttpEntity<String> request = new HttpEntity<>("{\"sentence\":\"" + sentence + "\"}", headers);
-//
-//        return restTemplate.postForObject(pythonServiceUrl, request, PredictionResult.class);
-//    }
-//}
-//
-//public class PredictionResult {
-//    private String Default;
-//    private String Offensive;
-//    private String Hate;
-//    private String Category;
-//
-//    // Getters and setters
-//}
+package com.ssafy.ssam.domain.profanity.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.ssafy.ssam.domain.profanity.dto.PredictionResultDto;
+
+@Service
+public class ProfanityService {
+    private final RestTemplate restTemplate;
+    private final String pythonServiceUrl = "http://localhost:8082/predict";
+
+    @Autowired
+    public ProfanityService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public PredictionResultDto getPrediction(String message) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        String requestBody = "{\"sentence\":\"" + message + "\"}";
+        HttpEntity<String> request = new HttpEntity<>(requestBody, headers);
+
+        try {
+            return restTemplate.postForObject(pythonServiceUrl, request, PredictionResultDto.class);
+        } catch (Exception e) {
+            // 예외 처리: 로깅 또는 사용자 정의 예외 throw
+            e.printStackTrace();
+            return new PredictionResultDto(); // 또는 null 반환 또는 사용자 정의 예외 throw
+        }
+    }
+}

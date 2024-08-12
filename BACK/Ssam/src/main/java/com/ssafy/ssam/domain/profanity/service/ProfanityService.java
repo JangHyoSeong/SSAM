c18e065a3d3570a9edf05548aafd827a4d423fa6
JJH <asz232@naver.com>
2024-08-12 12:26:12 +0900

    fix: ProfanityService.java

--- BACK/Ssam/src/main/java/com/ssafy/ssam/domain/profanity/service/ProfanityService.java
+++ BACK/Ssam/src/main/java/com/ssafy/ssam/domain/profanity/service/ProfanityService.java
@@ -4,19 +4,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.ssafy.ssam.domain.profanity.dto.PredictionResultDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
 public class ProfanityService {
     private final RestTemplate restTemplate;
     private final String pythonServiceUrl = "http://i11e201.p.ssafy.io:8082/predict";
+    private final ObjectMapper objectMapper;
 
     @Autowired
-    public ProfanityService(RestTemplate restTemplate) {
+    public ProfanityService(RestTemplate restTemplate, ObjectMapper objectMapper) {
         this.restTemplate = restTemplate;
+        this.objectMapper = objectMapper;
     }
 
     public PredictionResultDto getPrediction(String message) {
@@ -34,16 +38,19 @@ public class ProfanityService {
 
         try {
             System.out.println("Sending request to: " + pythonServiceUrl);
-            PredictionResultDto result = restTemplate.postForObject(pythonServiceUrl, request, PredictionResultDto.class);
-            System.out.println("Received response: " + result.getCategory());
-            System.out.println("Received response: " + result.getDefault());
-            System.out.println("Received response: " + result.getHate());
-            System.out.println("Received response: " + result.getOffensive());
+            ResponseEntity<String> responseEntity = restTemplate.postForEntity(pythonServiceUrl, request, String.class);
+            
+            String responseBody = responseEntity.getBody();
+            System.out.println("Received response body: " + responseBody);
+
+            PredictionResultDto result = objectMapper.readValue(responseBody, PredictionResultDto.class);
+            System.out.println("Parsed result: " + result);
+            
             return result;
         } catch (Exception e) {
             System.out.println("Exception occurred: " + e.getMessage());
             e.printStackTrace();
-            return new PredictionResultDto(); // 또는 null 반환 또는 사용자 정의 예외 throw
+            return new PredictionResultDto();
         }
     }
 }

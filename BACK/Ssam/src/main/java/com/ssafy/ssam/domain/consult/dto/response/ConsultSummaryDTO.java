c0b364a1434f7ef3d8fc8592c16ea383551188d5
JHS <wkdgytmd200@naver.com>
2024-08-13 10:51:05 +0900

    FIX : add consult id in student detail
    * fix : 학생 상세 정보 페이지, 상담 리스트에 아이디 포함

--- BACK/Ssam/src/main/java/com/ssafy/ssam/domain/consult/dto/response/ConsultSummaryDTO.java
+++ BACK/Ssam/src/main/java/com/ssafy/ssam/domain/consult/dto/response/ConsultSummaryDTO.java
@@ -11,6 +11,7 @@ import java.time.LocalDate;
 @AllArgsConstructor
 public class ConsultSummaryDTO {
 
+    private Integer consultId;
     private LocalDate date;
     private Integer runningTime;
     private String consultType;

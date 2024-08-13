e115ad81e5417ba3a3e831c6c68797d74485368d
박범준 <fizd7@naver.com>
2024-08-13 17:05:55 +0900

    feat: videoApi

--- FE/Frontend/src/apis/stub/72-75 상담요약/apiStubReservationSummary.jsx
+++ FE/Frontend/src/apis/stub/72-75 상담요약/apiStubReservationSummary.jsx
@@ -1,1 +1,18 @@
-// hi
+// 데이터 요청
+import axios from "axios";
+const apiUrl = import.meta.env.API_URL;
+
+export const fetchApiReservationSummary = async () => {
+  const token = localStorage.getItem("USER_TOKEN");
+  const response = await axios.get(`${apiUrl}/v1/consults/check`, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: token,
+    },
+  });
+  console.log("비디오 토큰", response.data);
+  return {
+    consultId: response.data.consultId,
+    accessCode: response.data.accessCode,
+  };
+};
